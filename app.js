c364afc7d96ca145cac13dbe3027f93715e673c5
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-12-18 20:45:19 +0500

    modified app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // const CatMe = require('cat-me') ;
 // console.log(CatMe()  );
 
+// const http = require('http') ;
+// const server = http.createServer((req, res)=>{
+//     res.end('server')
+// })
+// // run the server
+// server.listen(3000,()=>{
+//       log
+// })
+
 const http = require('http') ;
 const server = http.createServer((req, res)=>{
-    res.end('server')
-})
-// run the server
-server.listen(3000,()=>{
-     
+    res.sendDate('server')
 })
 
+server.listen(3000,()=>{
+    console.log('serevr is listening on the port 3000');
+    
+})

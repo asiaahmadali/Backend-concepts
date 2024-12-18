9d2a8986dd20460964921bebdf34f84dbb14452c
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-12-18 20:48:35 +0500

    reverse changes

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 // const CatMe = require('cat-me') ;
 // console.log(CatMe()  );
 
-// const http = require('http') ;
-// const server = http.createServer((req, res)=>{
-//     res.end('server')
-// })
-// // run the server
-// server.listen(3000,()=>{
-//       log
-// })
-
 const http = require('http') ;
 const server = http.createServer((req, res)=>{
-    res.sendDate('server')
+    res.end('server')
+})
+// run the server
+server.listen(3000,()=>{
+     
 })
 
-server.listen(3000,()=>{
-    console.log('serevr is listening on the port 3000');
-    
-})

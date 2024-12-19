3c45a2431010cfcd7ebb94c08c10b5229587afba
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-12-19 11:42:28 +0500

    change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http') ;
 const server = http.createServer((req, res)=>{
     res.end('server')
 })
-// run the server
+// run the server on port no 3000
 server.listen(3000,()=>{
      console.log("server is listening at port no 3000");
      

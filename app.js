989708e263c7115bba1a5c9f9bf9a86d8490759f
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-12-21 18:28:38 +0500

    express added

--- app.js
+++ app.js
@@ -3,7 +3,13 @@
 // call-me is a package that give code for cat
 const http = require('http') ;
 const server = http.createServer((req, res)=>{
-    res.end('server')
+    if(req.url=='/about'){
+        res.end('about page is here')
+    }
+    if(req.url=='/profile'){
+        res.end('profile page is here')
+    }
+    res.end('default page')
 })
 // run the server on port no 3000
 server.listen(3000,()=>{
